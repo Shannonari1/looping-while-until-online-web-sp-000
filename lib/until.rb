ec8703958c8bbd8f157798b3b86ba38d4ff4cf6d
Shannon Young <shannon.ari1@gmail.com>
2020-11-22 21:33:26 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,11 +1,8 @@
 
 def using_until
   levitation_force = 6
-  7.times do
-  	  puts "Wingardium Leviosa"
-
-  until levitation_force == 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1
-  end
+        until levitation_force == 10
+          puts "Wingardium Leviosa"
+          levitation_force += 1
+        end
 end
